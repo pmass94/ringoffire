0d59c39edd4421007f2889cc5fe7b537bc952e85
Peter Mass <mass.peter@web.de>
2021-09-18 07:21:51 +0200

    game-component

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+
+  //start game
+  newGame(){
+
+    this.router.navigateByUrl('/game');
+  }
+
+
+
 
 }
