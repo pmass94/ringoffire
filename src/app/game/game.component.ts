ddfe94f2bd90e4a3f98d77602b41bbcf5dfcac02
Peter Mass <mass.peter@web.de>
2021-09-21 08:33:36 +0200

    version update

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 @Component({
   selector: 'app-game',
@@ -8,22 +10,46 @@ import { Game } from 'src/models/game';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  
+  currentCard: string = '';
+  game: Game;
 
-  constructor() { }
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
   }
 
-  newGame(){
+  newGame() {
+    this.game = new Game();
 
   }
 
 
 
   takeCard() {
+    if (!this.pickCardAnimation) // if this.pickCardAnimation false (!)
+      this.currentCard = this.game.stack.pop();
     this.pickCardAnimation = true;
+
+    console.log('New card:' + this.currentCard) //current card on the board
+    console.log('Game is', this.game);
+
+    setTimeout(() => {
+      this.game.playedCards.push(this.currentCard);
+      this.pickCardAnimation = false;
+    }, 1000);
+  }
+
+
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed' , result);
+
+    });
   }
 }
 
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Jünther' , 'Olaf' , 'Harald'];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
@@ -12,8 +12,27 @@ export class Game {
             this.stack.push('diamonds_' + i);
         }
 
-      
+        shuffle(this.stack);
     }
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
 
 
