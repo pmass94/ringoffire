57591a26ef4ec82fbe1b149d4009ee299d1a9653
Peter Mass <mass.peter@web.de>
2021-09-25 09:32:47 +0200

    version update

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -5,6 +5,7 @@ import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
 import { EditPlayerComponent } from '../edit-player/edit-player.component';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-game',
@@ -15,6 +16,11 @@ export class GameComponent implements OnInit {
 
   game: Game;
   gameId: string
+  gameOver = false;
+
+  topMarginForPlayerNames = 1; // In REM
+  marginBetweenNames = 5; // In REM
+
 
 
 
@@ -39,6 +45,7 @@ export class GameComponent implements OnInit {
           this.game.currentPlayer = game.currentPlayer;
           this.game.playedCards = game.playedCards;
           this.game.players = game.players;
+          this.game.player_images = game.player_images;
           this.game.stack = game.stack;
           this.game.pickCardAnimation = game.pickCardAnimation;
           this.game.currentCard = game.currentCard;
@@ -47,17 +54,35 @@ export class GameComponent implements OnInit {
 
   }
 
-  /** Subscribe to game-changes in database and update local game object   
+  /**
+  * Set margins for player-display based on window-width & height
+  */
+  @HostListener('window:resize', ['$event'])
+  arrangePlayerDisplay() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    if (width < 450 || height < 600) (this.setSmallUi())
+    else if (width < 1200) (this.setMediumUi())
+    else (this.setLargeUi())
 
-  processGameUpdates() {
-    this.firestore
-      .collection('games') // access 'games' collection in firebase
-      .doc(this.gameId) // refer to a specific game via game-id
-      .valueChanges()
-      .subscribe((game) => {
-        console.log('Game update', game);
-      });
-  }*/
+  }
+
+  setSmallUi() {
+    this.marginBetweenNames = 2.75;
+    this.topMarginForPlayerNames = 0.5;
+  }
+
+  setMediumUi() {
+    this.marginBetweenNames = 4.5;
+    this.topMarginForPlayerNames = 1;
+  }
+
+  setLargeUi() {
+    this.topMarginForPlayerNames = 3.5;
+    this.marginBetweenNames = 6;
+  }
+
+
 
   newGame() {
     this.game = new Game();
@@ -67,7 +92,9 @@ export class GameComponent implements OnInit {
 
 
   takeCard() {
-    if (!this.game.pickCardAnimation) // if this.pickCardAnimation false (!)
+    if (this.game.stack.length == 0) {
+      this.gameOver = true;
+    } else if (!this.game.pickCardAnimation) // if this.pickCardAnimation false (!)
       this.game.currentCard = this.game.stack.pop();
     this.game.pickCardAnimation = true;
     console.log('New card:' + this.game.currentCard) //current card on the board
@@ -89,9 +116,18 @@ export class GameComponent implements OnInit {
 
     dialogRef.afterClosed().subscribe((change: string) => {
       console.log('Received change', change);
+      if (change) {
+        if (change == 'DELETE') {
+          this.game.players.splice(playerId, 1);
+          this.game.player_images.splice(playerId, 1);
+        } else {
+          this.game.player_images[playerId] = change;
+        }
+        this.saveGame();
+      }
     });
 
-   }
+  }
 
   /**
    * Open dialog to add player
@@ -103,6 +139,7 @@ export class GameComponent implements OnInit {
     dialogRef.afterClosed().subscribe((name: string) => {
       if (name && name.length > 0) {
         this.game.players.push(name);
+        this.game.player_images.push('1.png');
         this.saveGame();
       }
     });
